090d30f130459ab22cb55e6cde138dcd13f53a5e
Diego Osorio <diegoso-0989@hotmail.com>
2020-02-24 21:29:48 -0500

    Componente tabla, conectando el estado al componente tabla

--- src/components/Usuarios/index.js
+++ src/components/Usuarios/index.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import * as usuariosActions from '../../actions/usuariosActions';
 import Spinner from '../General/Spinner';
 import Fatal from '../General/Fatal';
+import Tabla from '../Usuarios/Tabla'
 
 class Usuarios extends Component {
 
@@ -12,57 +13,20 @@ class Usuarios extends Component {
 
   ponerContenido = () => {
     if(this.props.cargando) {
-      return (
-        <Spinner />
-      )
+      return <Spinner />
     }
-
     if(this.props.error) {
       return <Fatal mensaje= {this.props.error}/>
     }
 
-    return(
-      <table className="tabla">
-        <thead>
-          <tr>
-            <th>
-              Nombre
-            </th>
-            <th>
-              Correo
-            </th>
-            <th>
-              Enlace
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          { this.ponerFilas() }
-        </tbody>
-      </table>
-    )
+    return <Tabla />
   }
-
-	ponerFilas = () => (
-    this.props.usuarios.map(usuario => (
-      <tr key={usuario.id}>
-        <td>
-          { usuario.name }
-        </td>
-        <td>
-          { usuario.email }
-        </td>
-        <td>
-          { usuario.website }
-        </td>
-      </tr>
-    ))
-  );
 
 	render (){
     console.log(this.props)
     return(
       <div>
+        <h1>Usuarios</h1>
         { this.ponerContenido() }
       </div>
     )
