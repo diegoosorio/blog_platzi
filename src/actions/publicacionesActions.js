119eabcd8c6f1f3a998d46d28d70863406b1d8ae
Diego Osorio <diegoso-0989@hotmail.com>
2020-03-01 18:03:37 -0500

    Inmutabilidad

--- src/actions/publicacionesActions.js
+++ src/actions/publicacionesActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
-import { TRAER_POR_USUARIO, CARGANDO, ERROR } from '../types/publicacionesTypes';
+import { TRAER_POR_USUARIO } from '../types/publicacionesTypes';
+import * as usuariosTypes from '../types/usuariosTypes';
+
+const { TRAER_TODOS: USUARIOS_TRAER_TODOS } = usuariosTypes;
 
 export const traerPorUsuario = (key) => async (dispatch, getState) => {
   const { usuarios } = getState().usuariosReducer;
@@ -13,8 +16,21 @@ export const traerPorUsuario = (key) => async (dispatch, getState) => {
     respuesta.data
   ]
 
+  const publicaciones_key = publicaciones_actualizadas.length - 1;
+  const usuarios_actualizados = [...usuarios];
+  
+  usuarios_actualizados[key] = {
+    ...usuarios[key],
+    publicaciones_key
+  }
+
+  dispatch({
+    type: USUARIOS_TRAER_TODOS,
+    payload: usuarios_actualizados
+  });
+
   dispatch({
     type: TRAER_POR_USUARIO, 
     payload: publicaciones_actualizadas
-  })
+  });
 } 
